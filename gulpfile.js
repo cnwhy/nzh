237f58e1600540879c5b5819e234b32966cd494e
hangyang.wang <w.why@163.com>
2017-05-17 20:01:53 +0800

    '增加自动浏览器测试'

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var path = require("path");
 var gulp = require("gulp");
-// var util = require("gulp-util");
 var mocha = require("gulp-mocha");
-// var istanbul = require("gulp-istanbul");
+var mochaPhantomjs = require("gulp-mocha-phantomjs");
 var browserify = require("gulp-browserify");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var header = require("gulp-header");
-
+// var istanbul = require("gulp-istanbul");
 var package = require("./package.json");
 var banner =
   '/*!\n' +
@@ -16,18 +15,14 @@ var banner =
   ' * License ' + package.license + '\n' +
   ' */\n'
 
-gulp.task('build',['test'],function(){
+gulp.task('build',['test-server'],function(){
 	return gulp.src('./browser-source/*.js')
 		.pipe(browserify())
 		.pipe(header(banner))
-		// .pipe(rename({
-		// 	basename:package.name
-		// }))
 		.pipe(gulp.dest('./dist'))
 		.pipe(uglify())
 		.pipe(header(banner))
 		.pipe(rename({
-			//basename:package.name+".min"
 			suffix:".min"
 		}))
 		.pipe(gulp.dest('./dist'));
@@ -51,9 +46,29 @@ gulp.task('build',['test'],function(){
 //     .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
 // });
 
-gulp.task('test', function () {
-  return gulp.src('test/test_mocha.js', {read: false})
-    .pipe(mocha({reporter: 'dot'}))
+gulp.task('test',['test-server','test-browser'])
+
+gulp.task('test-server', function () {
+	return gulp.src('test/test_mocha.js', {read: false})
+		.pipe(mocha({reporter: 'dot'}))
 });
 
-gulp.task('default', ['build']);
+gulp.task('output-testjs-browser',['test-server'],function(){
+	return gulp.src('test/test_mocha.js')
+		.pipe(browserify({insertGlobals:true}))
+		.pipe(rename({
+			basename:"tests"
+		}))
+		.pipe(gulp.dest('test/browser'));
+})
+
+gulp.task('test-browser',['output-testjs-browser','build'],function(){
+	return gulp.src('test/browser/test.html')
+		.pipe(mochaPhantomjs({reporter: 'dot'}))
+})
+
+gulp.task('test-docs',['build'],function(){
+	
+})
+
+gulp.task('default', ['build','test']);
